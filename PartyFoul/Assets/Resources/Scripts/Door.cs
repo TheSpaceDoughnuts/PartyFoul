c994c4aad8c60c48b189e2fe971416bdb0f98958
Jake Higgins <jth7036@rit.edu>
2015-01-24 00:15:35 -0500

    Added scene changing between LivingRoom and DiningRoom

---
+++ PartyFoul/Assets/Resources/Scripts/Door.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	public string targetRoom;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if(col.gameObject.name == "Player") {
+			Application.LoadLevel (targetRoom);
+		}
+	}
+}
