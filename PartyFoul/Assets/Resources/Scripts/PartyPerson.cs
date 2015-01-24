dc94a743576522326a1ed64e07844075d7484835
Jake Higgins <jth7036@rit.edu>
2015-01-23 23:14:20 -0500

    Added PartyPerson class as base class for all moving people.

---
+++ PartyFoul/Assets/Resources/Scripts/PartyPerson.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PartyPerson : MonoBehaviour {
+
+	private Vector2 _target = Vector2.zero;
+	private bool _hasTarget = false;
+
+	public Vector2 force;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		onUpdate();
+	}
+
+	protected virtual void onUpdate(){
+		SeekTarget();
+	}
+
+	public void SetTarget(Vector2 target) {
+		_target = target;
+		_hasTarget = true;
+	}
+
+	public void SeekTarget() {
+		if(_hasTarget) {
+			Vector2 dir = _target;
+			dir.x -= gameObject.transform.position.x;
+			dir.y -= gameObject.transform.position.y;
+			Vector2 norm = dir.normalized;
+			norm.Scale(force);
+			gameObject.rigidbody2D.AddForce (norm);
+		}
+	}
+}
---
+++ PartyFoul/Assets/Resources/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : PartyPerson {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	protected override void onUpdate ()
+	{
+		base.onUpdate ();
+		#region WASD Code
+		/*if(Input.GetKey(KeyCode.W)) {
+			input.y += force.y;
+		}
+		if(Input.GetKey(KeyCode.S)){
+			input.y -= force.y;
+		}
+		if(Input.GetKey(KeyCode.A)) {
+			input.x -= force.x;
+		}
+		if(Input.GetKey(KeyCode.D)) {
+			input.x += force.x;
+		}*/
+		#endregion
+		
+		if(Input.GetMouseButtonDown(1)) {
+			//1280x720
+			Vector2 target = Input.mousePosition;
+			
+			Vector2 rescale = new Vector2(target.x / Screen.width, 
+			                              target.y / Screen.height);
+			Vector2 size = new Vector2(6.5f, 3.5f);
+			Vector2 result = new Vector2(rescale.x * size.x*2,
+			                             rescale.y * size.y*2);
+			result.x -= size.x;
+			result.y -= size.y;
+			
+			SetTarget (result);
+		}
+	}
+}
