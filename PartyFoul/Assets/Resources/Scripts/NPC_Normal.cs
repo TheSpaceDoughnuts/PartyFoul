bd8f86eb975473528438fd51ca0047413d87b378
Tim Steen <tls8963@rit.edu>
2015-01-24 14:15:26 -0500

    -Added NPC Room -Added NPC_Normal Script, basic roaming -Added NPC Prefab

--- PartyFoul/Assets/Resources/Scripts/NPC_Normal.cs
+++ PartyFoul/Assets/Resources/Scripts/NPC_Normal.cs
@@ -1,7 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NPC_Normal : MonoBehaviour {
+public class NPC_Normal : PartyPerson {
+	float elapsedTime;
+	public float waitTargetTime = 1;
 
-
+	protected override void OnUpdate ()
+	{
+		elapsedTime += Time.deltaTime;
+		if(elapsedTime > waitTargetTime){
+			elapsedTime = 0;
+			SetTarget(gameObject.transform.position * -1);
+		}
+		
+		SeekTarget();
+	}
 }
--- PartyFoul/Assets/Resources/Scripts/PartyPerson.cs
+++ PartyFoul/Assets/Resources/Scripts/PartyPerson.cs
@@ -2,37 +2,39 @@
 using System.Collections;
 
 public class PartyPerson : MonoBehaviour {
-
+	
 	private Vector2 _target = Vector2.zero;
 	private bool _hasTarget = false;
-
+	
 	public Vector2 force;
 	public Vector3 scale;
-
+	public float arrivalDistance = 0.1f;
+	
 	// Use this for initialization
 	void Start () {
 		OnStart ();
 	}
-
+	
 	protected virtual void OnStart() {
-		Debug.Log ("start");
 		gameObject.transform.localScale = scale;
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
 		OnUpdate();
 	}
-
+	
 	protected virtual void OnUpdate(){
 		SeekTarget();
+		
+		
 	}
-
+	
 	public void SetTarget(Vector2 target) {
 		_target = target;
 		_hasTarget = true;
 	}
-
+	
 	public void SeekTarget() {
 		if(_hasTarget) {
 			Vector2 dir = _target;
@@ -42,5 +44,25 @@ public class PartyPerson : MonoBehaviour {
 			norm.Scale(force);
 			gameObject.rigidbody2D.AddForce (norm);
 		}
+	}
+	
+	public bool AttemptArrival() {
+		if(_hasTarget) {
+			float distance = Distance(gameObject.transform.position, _target);
+			if(distance < arrivalDistance) {
+				_hasTarget = false;
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public float Distance(Vector2 start, Vector2 end) {
+		float dx = (end.x - start.x) * (end.x - start.x);
+		float dy = (end.y - start.y) * (end.y - start.y);
+		
+		float distance = Mathf.Sqrt (dx + dy);
+		
+		return distance;
 	}
 }
